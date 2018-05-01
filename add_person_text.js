c915a5f66cc2f51c022129371ea215e67e090e8e
Devon Norris <norris.devon@gmail.com>
2018-05-01 18:53:11 +0000

    knex implementation

---
+++ add_person_text.js
@@ -0,0 +1,20 @@
+const settings = require("./settings.json");
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user       : settings.user,
+    password   : settings.password,
+    database   : settings.database,
+    host       : settings.hostname,
+    port       : settings.port,
+    ssl        : settings.ssl
+  }
+});
+
+knex('famous_people')
+    .insert({first_name: process.argv[2],
+             last_name: process.argv[3],
+             birthdate: process.argv[4]})
+    .then(console.log("done"))
+    .catch(err => console.log("error: ", err))
+    .finally(destroy => knex.destroy());
